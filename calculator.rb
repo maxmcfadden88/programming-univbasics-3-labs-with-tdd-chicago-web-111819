aff28aec26b28085ed4e0c9385df672168d66943
Max McFadden <maxmcfadden88@gmail.com>
2019-11-08 17:04:02 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,12 +1,5 @@
-do 
-  
-  def first_number(first_number = 10)
-  puts #{first_number}
-end
-
-def second_number(second_number = 2)
-  puts #{second_number}
-  end
+def first_number = 10
+def second_number = 2 
 
 first_number
 second_number
