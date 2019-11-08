55854f272f6c29ff743ebbe3cb97f12794797e39
Max McFadden <maxmcfadden88@gmail.com>
2019-11-08 18:57:12 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,32 @@
-# Add your variables here
+./calculator.rb
+
+def first_number
+  first_number = 10
+  puts first_number
+end
+
+def second_number
+  second_number = 2
+  puts second_number
+end
+
+def sum (num1, num2) 
+   puts num1 + num2
+end
+
+def difference (num1, num2) 
+   puts num1 - num2
+end
+
+def product (num1, num2) 
+   puts num1 * num2
+end
+
+def quotient (num1, num2) 
+   puts num1 / num2
+end
+
+sum(10,2)
+difference(10,2)
+product(10,2)
+quotient(10,2)
